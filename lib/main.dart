a555ffec20f77bb1c20be8c3fbfd8187b3a1191f
dido <dido@macbook-pro-de-dido.home>
2018-04-01 00:21:12 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    home: new TextWidget(),
+  ));
+}
+
+class TextWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Center(
+        child: new Container(
+          color: Colors.red[200],
+          width: 200.0,
+          height: 100.0,
+
+          child: new Center(
+              child: new Icon(Icons.favorite,color: Colors.yellow,)
+
+          ), // center
+        ), //container
+      ), //center
+    ); // scaffold
+  }
+}
